2e2795674c4e639ef02f7682a5ed47ff34837235
ohad aizik <48066182+oaizik@users.noreply.github.com>
2020-02-27 17:54:03 +0200

    Update Sentences.js

--- src/Components/CatalogPage/Sentences.js
+++ src/Components/CatalogPage/Sentences.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import { IconButton, Tooltip } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { IconButton, Tooltip, makeStyles } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import FavoriteBorderTwoToneIcon from '@material-ui/icons/FavoriteBorderTwoTone';
 import { connect } from 'react-redux';
 import { fetchSentences } from '../../redux/actions/sentenceActions';
-import './sentences.css';
 import store from '../../redux/store';
 
-class Sentences extends Component {
+const useStyles = makeStyles(theme => ({
+    sentencescontainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    }
+}));
 
-    componentDidMount() {
+const Sentences = props => {
+    const classes = useStyles();
+    const [content, setContent] = useState(<div></div>);
+
+    useEffect(() => { 
+        props.fetchSentences();
         const state = store.getState();
-        if(state.sentences.items.length === 0) {
-            this.props.fetchSentences();
-        }
-    };
+        if(Object.keys(state.sentences.item).length !== 0) {
+            props.sentences.unshift(state.sentences.item);
+        };
+        const display = props.sentences.map(sentence => (
+            <div key={sentence.sentenceId}
+                style={{borderRadius: '10px', border: '2px solid', margin: '10px', height: '300px', width: '350px', backgroundColor: sentence.style.backgroundColor,}}>
+                <div style={{height: '255px', fontSize: '22px', display: 'flex', color: sentence.style.textColor, fontFamily: sentence.style.fontFamily, fontWeight: sentence.style.fontWeight, fontStyle: sentence.style.fontStyle, textDecoration: sentence.style.textDecoration, textAlign: sentence.style.textAlign, alignItems: sentence.style.alignItems }}>{sentence.sentenceBody}</div>
+                <div className="iconsdiv" style={{height: '45px', backgroundColor: 'white', display: 'flex', justifyContent: 'space-between'}}>
+                    <Tooltip title="like" aria-label="like" >
+                        <IconButton aria-label="add to shopping cart">
+                            <FavoriteBorderTwoToneIcon style={{ color: 'red' }} />{sentence.numOfOrders} 
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="edit" aria-label="edit" >
+                        <NavLink to = {{
+                            pathname:'/Editor',
+                            params: sentence.sentenceId}}
+                        >
+                            <EditOutlinedIcon style={{ color: 'gray', marginTop: '10px' }} />
+                        </NavLink>
+                    </Tooltip>
+                    <Tooltip title="buy now" aria-label="buy now" >
+                        <IconButton aria-label="add to shopping cart" onClick={() => buyClicked(sentence.sentenceId)} >
+                            <AddShoppingCartIcon style={{ color: 'gray' }} />
+                        </IconButton>
+                    </Tooltip>
+                </div>
+            </div>
+        ));
+        setContent(display);
+    }, []);
 
-    likeClicked(e) {
-        console.log(e);
-    };
-
-    async buyClicked(sentenceId) {
+    const buyClicked = async(sentenceId) => {
         if(localStorage.getItem('userId')) { 
             //  new order
             let parsedRes;
@@ -36,16 +69,15 @@ class Sentences extends Component {
                                            sentenceId: sentenceId,
                                            platform: "canvas",
                                            style: {
-                                            textColor: this.props.sentences[sentenceId].style.textColor,
-                                            backgroundColor: this.props.sentences[sentenceId].style.backgroundColor,
-                                            fontFamily: this.props.sentences[sentenceId].style.fontFamily,
-                                            fontSize: this.props.sentences[sentenceId].style.fontSize,
-                                            fontWeight: this.props.sentences[sentenceId].style.fontWeight,
-                                            fontStyle: this.props.sentences[sentenceId].style.fontStyle,
-                                            textDecoration: this.props.sentences[sentenceId].style.textDecoration,
-                                            textAlign: this.props.sentences[sentenceId].style.textAlign,
-                                            alignItems: this.props.sentences[sentenceId].style.alignItems } 
-                                            // alignItems: state.sentences.items[sentenceId].style.alignItems
+                                            textColor: props.sentences[sentenceId].style.textColor,
+                                            backgroundColor: props.sentences[sentenceId].style.backgroundColor,
+                                            fontFamily: props.sentences[sentenceId].style.fontFamily,
+                                            fontSize: props.sentences[sentenceId].style.fontSize,
+                                            fontWeight: props.sentences[sentenceId].style.fontWeight,
+                                            fontStyle: props.sentences[sentenceId].style.fontStyle,
+                                            textDecoration: props.sentences[sentenceId].style.textDecoration,
+                                            textAlign: props.sentences[sentenceId].style.textAlign,
+                                            alignItems: props.sentences[sentenceId].style.alignItems } 
                                         }),
                     })
                     parsedRes = await response.json();
@@ -90,40 +122,12 @@ class Sentences extends Component {
             alert('you have to be logged in before making a purches!');
         }
     };
-    
-    render() {
-        let liked = 'gray';
-        const sentenceItems = this.props.sentences.map(sentence => (
-            <div key={sentence.sentenceId} style={{border: '1px solid', margin: '10px', height: '250px', width: '300px', backgroundColor: sentence.style.backgroundColor,}}>
-                <div style={{height: '205px', fontSize: '18px', display: 'flex', color: sentence.style.textColor, fontFamily: sentence.style.fontFamily, fontWeight: sentence.style.fontWeight, fontStyle: sentence.style.fontStyle, textDecoration: sentence.style.textDecoration, textAlign: sentence.style.textAlign, alignItems: sentence.style.alignItems }}>{sentence.sentenceBody}</div>
-                <div className="iconsdiv" style={{height: '45px', backgroundColor: 'white', display: 'flex', justifyContent: 'space-between',}}>
-                    <Tooltip title="like" aria-label="like" >
-                        <IconButton aria-label="add to shopping cart" onClick={() => this.likeClicked(this)}>
-                            <FavoriteBorderTwoToneIcon style={{ color: liked }} />
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="edit" aria-label="edit" >
-                        <NavLink to = {{
-                            pathname:'/Editor',
-                            params: sentence.sentenceId}}
-                        >
-                            <EditOutlinedIcon style={{ color: 'gray', marginTop: '10px' }} />
-                        </NavLink>
-                    </Tooltip>
-                    <Tooltip title="buy now" aria-label="buy now" >
-                        <IconButton aria-label="add to shopping cart" onClick={() => this.buyClicked(sentence.sentenceId)} >
-                            <AddShoppingCartIcon style={{ color: 'gray' }} />
-                        </IconButton>
-                    </Tooltip>
-                </div>
-            </div>
-        ));
-            return (
-                <div className="sentencescontainer" >
-                    {sentenceItems}
-                </div>
-        );
-    }
+            
+    return (
+        <div className={classes.sentencescontainer} >
+            {content}
+        </div>
+    );
 }
 
 const mapStateToProps = state => ({
@@ -132,5 +136,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, { fetchSentences })(Sentences);
-
-
